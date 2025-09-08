c3ae654a49e7b7ddd787c3a2f568fe17350bda38
peep123123 <dovmf5258@gmail.com>
2025-09-08 22:33:35 +0900

    feat: complete basic CRUD API

--- not-to-do-list-backend/src/main/java/com/example/not_to_do_list_backend/controller/NotTodoController.java
+++ not-to-do-list-backend/src/main/java/com/example/not_to_do_list_backend/controller/NotTodoController.java
@@ -1,5 +1,7 @@
 package com.example.not_to_do_list_backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,5 +35,55 @@ public class NotTodoController {
 		// HTTP 응답으로 저장된 객체를 반환 
         // 상태 코드 200(성공)과 함께 응답
 		return ResponseEntity.ok(savedTodo);
+	}
+	
+	// HTTP GET 요청을 처리하는 메소드
+	@GetMapping
+	public ResponseEntity<List<NotTodo>> getAllNotTodo() {
+		
+		// 데이터베이스에 저장된 모든 'NotTodo' 목록을 조회
+		List<NotTodo> todos = notTodoRepository.findAll();
+		return ResponseEntity.ok(todos);
+	}
+	
+	// HTTP DELETE 요청을 처리하는 메소드임을 스프링에게 알려줌
+	// id는 URL 경로에서 id 값을 받겠다는 뜻 
+	@DeleteMapping("/{id}")
+	
+	//@PathVariable: URL 경로에 있는 id 값을 메소도의 매개변수 id로 가져옴
+	//Void(클래스): 제네릭 클래스는 기본 타입을 사용할 수 없고, 객체 타입만 허용
+	//반환할 데이터가 없을 때 Void 객체를 사용하여 명시적으로 선언함
+	public ResponseEntity<Void> deleteNotTodo(@PathVariable Long id){
+		
+		//매개변수로 받은 ID에 해당하는 데이터를 데이터베이스에서 삭제
+		notTodoRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	//PUT 요청을 처리하며, URL의 {id} 값을 받아옴
+	@PutMapping("/{id}")
+	
+	//@RequestBody NotTodo updatedTodo: 요청 본문(JSON)에 담겨온 수정할 데이터를 updatedTodo 객체에 매핑
+	public ResponseEntity<NotTodo> updateNotTodo(@PathVariable Long id, @RequestBody NotTodo updateTodo) {
+		
+		//.map(): findById()로 항목을 찾았을 경우, 람다식 내부의 코드를 실행하여 데이터 수정 및 저장
+		return notTodoRepository.findById(id)
+				.map(todo -> {
+					todo.setTitle(updateTodo.getTitle());
+					todo.setCompleted(updateTodo.isCompleted());
+					NotTodo savedTodo = notTodoRepository.save(todo);
+					return ResponseEntity.ok(savedTodo);
+				})
+				//.orElseGet(): findById()로 항목을 찾지 못했을 경우, 404 에러 반환
+				.orElseGet(() -> ResponseEntity.notFound().build()); 
+	}
+	
+	// id로 단일 항목 조회하기
+	@GetMapping("/{id}")
+	public ResponseEntity<NotTodo> getNotTodoById(@PathVariable Long id) {
+		
+		return notTodoRepository.findById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
